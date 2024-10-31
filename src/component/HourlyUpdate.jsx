4b5f27688019e6964688a41c358b39e6ac5b893c
RITIK-SAINI23 <ritik23saini@gmail.com>
2024-10-31 20:34:16 +0530

    2

--- src/component/HourlyUpdate.jsx
+++ src/component/HourlyUpdate.jsx
@@ -14,16 +14,16 @@ const HourlyUpdate = () => {
     const { ForcastData } = useContext(weatherContext);
 
     return (
-        <div className=" md: h-[40%] flex flex-col  bg-purple-300 mt-5 rounded-2xl text-xl">
+        <div className=" md: h-[40%] flex flex-col  bg-purple-300 mt-3 rounded-2xl text-xl">
             {/* Header */}
-            <div className="mt-3 mx-2 p-2 font-bold text-xl">Hourly Updates</div>
+            <div className=" mx-2 p-2  mt-5 font-bold text-xl">Hourly Updates</div>
 
             {/* Content taking remaining height */}
             <div className='flex-grow flex justify-evenly items-center '>
                 {ForcastData.Hourlyforecast.map((data) => {
                     const time = getTime(data.dt_txt);
                     return (
-                        <div key={data.dt} className='rounded-xl gap-1 p-1 md:gap-7 bg-white sm:m-2 sm:p-4 inline-grid md:grid items-center'>
+                        <div key={data.dt} className=' rounded-lg  md:gap-7 bg-white sm:m-2 sm:p-4 inline-grid md:grid items-center'>
                             <div className="text-sm font-bold">{time}</div>
                             <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} alt={data.weather[0].description} />
                             <div className="text-sm lg:text-lg font-bold">{data.main.temp}°C</div>
